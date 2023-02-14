de45a875993533c6cacef0eca4bb530d8ce641b2
Uria <ulevi92@gmail.com>
2023-02-14 14:45:44 +0200

    fixed/ topbuttons won't show up

--- src/components/buttons/top/TopButtons.tsx
+++ src/components/buttons/top/TopButtons.tsx
@@ -1,47 +1,48 @@
-import {
-  IonButton,
-  IonButtons,
-  IonCol,
-  IonIcon,
-  IonRow,
-  IonToolbar,
-} from "@ionic/react";
+import { IonButton, IonButtons, IonIcon, IonToolbar } from "@ionic/react";
 import { onButtonTouch } from "../generalButton.helper";
+
 import {
   backspaceOutline,
   backspaceSharp,
   timeOutline,
   timeSharp,
 } from "ionicons/icons";
+
 const TopButtons = () => {
   return (
-    <IonRow>
-      <IonCol>
-        <IonToolbar>
-          <IonButtons slot='end' class='ion-paddin-x-3'>
-            <IonButton shape='round' fill='clear' onTouchStart={onButtonTouch}>
-              <IonIcon
-                name='backspace'
-                color='primary'
-                md={backspaceSharp}
-                ios={backspaceOutline}
-              />
-            </IonButton>
-          </IonButtons>
+    <IonToolbar>
+      <IonButtons slot='end'>
+        <IonButton
+          shape='round'
+          fill='clear'
+          onTouchStart={onButtonTouch}
+          class='ion-paddin-x-3'
+        >
+          <IonIcon
+            color='primary'
+            md={backspaceSharp}
+            ios={backspaceOutline}
+            size='large'
+          />
+        </IonButton>
+      </IonButtons>
 
-          <IonButtons slot='start' class='ion-padding-x-3'>
-            <IonButton shape='round' fill='clear' onTouchStart={onButtonTouch}>
-              <IonIcon
-                name='history'
-                color='secondary'
-                md={timeSharp}
-                ios={timeOutline}
-              />
-            </IonButton>
-          </IonButtons>
-        </IonToolbar>
-      </IonCol>
-    </IonRow>
+      <IonButtons slot='start'>
+        <IonButton
+          shape='round'
+          fill='clear'
+          onTouchStart={onButtonTouch}
+          class='ion-paddin-x-3'
+        >
+          <IonIcon
+            color='secondary'
+            md={timeSharp}
+            ios={timeOutline}
+            size='large'
+          />
+        </IonButton>
+      </IonButtons>
+    </IonToolbar>
   );
 };
 
